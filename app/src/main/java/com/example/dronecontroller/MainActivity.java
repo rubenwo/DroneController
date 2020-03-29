0dfade9f34863513d85770a9d428a6fdaa53a165
Ruben <rwoldhui@avans.nl>
2020-03-29 20:39:36 +0200

    [Updated] Android Studio + Project

--- app/src/main/java/com/example/dronecontroller/MainActivity.java
+++ app/src/main/java/com/example/dronecontroller/MainActivity.java
@@ -1,41 +1,17 @@
 package com.example.dronecontroller;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button up;
-    private Button left;
-    private Button right;
-    private Button down;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        up = findViewById(R.id.btn_up);
-        left = findViewById(R.id.btn_left);
-        right = findViewById(R.id.btn_right);
-        down = findViewById(R.id.btn_down);
 
-        up.setOnClickListener(v -> {
-
-        });
-
-        left.setOnClickListener(v -> {
-
-        });
-
-        right.setOnClickListener(v -> {
-
-        });
-
-        down.setOnClickListener(v -> {
-
-        });
     }
 }
